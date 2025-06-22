15dd393927822ce0fc4a3535a431b12c7819e289
Memfis <yura.kondratuk@gmail.com>
2025-06-22 19:37:29 +0200

    first commit

---
+++ EnvDocker_run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+KLIPPER_HOST_PATH="$(cd "$SCRIPT_DIR/.." && pwd)"
+HISTORY_FILE="$SCRIPT_DIR/EnvDocker_bash_hist.txt"
+
+echo "==> Монтируем: $KLIPPER_HOST_PATH в /klipper"
+echo "==> История команд: $HISTORY_FILE"
+
+HOST_UID=$(id -u)
+HOST_GID=$(id -g)
+
+DOCKER_RUN_OPTS=(
+  -it --rm
+  -u $HOST_UID:$HOST_GID
+  -e TERM=xterm-256color
+  -v "$KLIPPER_HOST_PATH:/klipper"
+)
+
+# Если файл истории существует — монтируем и подгружаем его
+if [ -f "$HISTORY_FILE" ]; then
+#  echo "==> История команд найдена: $HISTORY_FILE"
+  DOCKER_RUN_OPTS+=(-v "$HISTORY_FILE:/home/klippy/init_history.sh:ro")
+fi
+
+if [ $# -eq 0 ]; then
+  CMD="bash"
+else
+  CMD=$(printf "%q " "$@")
+fi
+
+# echo "$SCRIPT_DIR"
+# echo "$KLIPPER_HOST_PATH"
+# echo "$HISTORY_FILE"
+# echo "$CONTAINER_CMD"
+# echo "${DOCKER_RUN_OPTS[@]}"
+
+#set -x
+echo "==> Запуск интерактивной оболочки"
+docker run ${DOCKER_RUN_OPTS[@]} klipper-build-env bash -c "$CMD"
---
+++ SimDocker_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+echo $SCRIPT_DIR
+TARGET_UID=$(id -u)
+TARGET_GID=$(id -g)
+
+sudo usermod -aG docker $USER
+
+docker build \
+  --build-arg TARGET_UID=$TARGET_UID \
+  --build-arg TARGET_GID=$TARGET_GID \
+  -f "$SCRIPT_DIR/SimDocker_file" \
+  -t klipper-simulator-env "$SCRIPT_DIR/.."
